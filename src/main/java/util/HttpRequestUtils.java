f324e705a066e10cf9155dcd0a8e526d77453dac
kimdongmin <meena2003@naver.com>
2023-04-05 13:44:52 +0900

    refactor(mission2): HttpRequestUtils 클래스 내부 코드를 리팩토링
    RequestLine, RequestHeader 클래스를 멤버변수로 가지고, 각 클래스 기능을 사용할 메소드 구현

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -1,8 +1,37 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHeader;
+import webserver.RequestLine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class HttpRequestUtils {
-    public static String getUrl(String requestLine) {
-        String[] tokens = requestLine.split(" ");
-        return tokens[1];
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestUtils.class);
+    private RequestLine requestLine;
+    private RequestHeader requestHeader;
+
+    public HttpRequestUtils(BufferedReader br) throws IOException {
+        requestLine = new RequestLine(br);
+        requestHeader = new RequestHeader(br);
     }
+
+    public void printHttpRequestAllHeaders() {
+        System.out.println(requestHeader.getRequestHeaders());
+    }
+
+    public String getRequestLineHttpURI() {
+        return requestLine.getHTTP_URI();
+    }
+
+    public String getRequestLineHttpVersion() {
+        return requestLine.getHTTP_VERSION();
+    }
+
+    public String getRequestLineHttpMethod() {
+        return requestLine.getHTTP_METHOD();
+    }
+
 }
