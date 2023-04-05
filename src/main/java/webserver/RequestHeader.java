59934b909d5a68017dace5e6535d4d50192930c6
kimdongmin <meena2003@naver.com>
2023-04-05 12:41:16 +0900

    feat(mission2): RequestHeader 메소드 추가
    Http Reqeust Header 값 반환 및 사이즈 값 반환 메소드 추가

--- src/main/java/webserver/RequestHeader.java
+++ src/main/java/webserver/RequestHeader.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 public class RequestHeader {
     private final HashMap<String, String> requestHeader;
@@ -14,26 +13,29 @@ public class RequestHeader {
         convertRequestHeaderInfoToMap(requestHeaderInfo);
     }
 
-    // requestHeader를 Header-Name과 Header-Value로 나누어 HashMap에 저장
-    private void convertRequestHeaderInfoToMap(BufferedReader br) throws IOException {
+    // requestHeader를 HeaderName과 HeaderValue로 나누어 HashMap에 저장
+    private void convertRequestHeaderInfoToMap(BufferedReader requestHeaderInfo) throws IOException {
         String headerLine;
-        while (!(headerLine = br.readLine()).equals("")) {
+        while ((headerLine = requestHeaderInfo.readLine()) != null) {
             String[] tokens = headerLine.split(": ");
             requestHeader.put(tokens[0], tokens[1]);
         }
     }
 
-    // requestHeader 전체를 출력
-    public void printRequestHeaderLog() {
+    public StringBuilder getRequestHeaders() {
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, String> entrySet : requestHeader.entrySet()) {
             sb.append(entrySet.getKey() + ": " + entrySet.getValue() + "\n");
         }
-        System.out.println(sb);
+        return sb;
     }
 
     // 해당하는 헤더 값을 반환
     public String getHeaderValue(String headerName) {
         return requestHeader.get(headerName);
+    }
+
+    public int getHeaderSize() {
+        return requestHeader.size();
     }
 }
