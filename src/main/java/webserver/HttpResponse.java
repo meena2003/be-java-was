8647f234a56e6cadc3a6c3ed2fbc548969405ab7
kimdongmin <meena2003@naver.com>
2023-04-13 19:22:43 +0900

    feat(mission3): FrontController에 css uri가 들어오면 css 파일을 response로 보내주는 기능 구현
    HttpResponse 클래스에 css만 처리하는 메소드 생성

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -11,16 +11,50 @@ import org.slf4j.LoggerFactory;
 public class HttpResponse {
     private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
     private final DataOutputStream dos;
-    private final String ROOT_PATH = "src/main/resources/templates";
+    private final String TEMPLATES_ROOT_PATH = "src/main/resources/templates";
+    private final String STATIC_ROOT_PATH = "src/main/resources/static";
 
     public HttpResponse(DataOutputStream dos) {
         this.dos = dos;
     }
 
-    public void response200Header(int lengthOfBodyContent) {
+    public void response200HeaderHTML(int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public void response200HeaderCSS(int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public void response200HeaderFavicon(int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: image/avif,image/webp,image/apng,image/svg+xml\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public void response200HeaderJs(int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: */*\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -37,7 +71,11 @@ public class HttpResponse {
         }
     }
 
-    public byte[] readFile(String uri) throws IOException {
-        return Files.readAllBytes(new File(ROOT_PATH + uri).toPath());
+    public byte[] readFileFromTemplates(String uri) throws IOException {
+        return Files.readAllBytes(new File(TEMPLATES_ROOT_PATH + uri).toPath());
+    }
+
+    public byte[] readFileFromStatic(String uri) throws IOException {
+        return Files.readAllBytes(new File(STATIC_ROOT_PATH + uri).toPath());
     }
 }
