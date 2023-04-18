dd1bce3c4d55bcf9dd0a71cd4829cc9bd9b0ba34
kimdongmin <meena2003@naver.com>
2023-04-18 17:29:27 +0900

    feat(mission3): 회원 가입 후 index.html 화면으로 이동하는 기능 구현
    redirect 적용하지 않음

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -21,24 +21,26 @@ public class FrontController {
             uri = "/index.html";
         }
 
+        if (uri.startsWith("/user/create")) {
+            String userId = httpRequest.getQueryParameter("userId");
+            String password = httpRequest.getQueryParameter("password");
+            String name = httpRequest.getQueryParameter("name");
+            String email = httpRequest.getQueryParameter("email");
+            UserService.join(userId, password, name, email);
+
+            // 다시 홈 화면을 출력한다.
+            uri = "/index.html";
+            ContentType contentType = ContentType.getContentType(uri);
+            ViewResolver.resolveView(uri, contentType, httpResponse);
+
+            byte[] body = httpResponse.readFile(contentType.getRootPath(), uri);
+            httpResponse.response200Header(body.length, contentType);
+            httpResponse.responseBody(body);
+            return;
+        }
+
+        // 요청받은 파일을 처리한다
         ContentType contentType = ContentType.getContentType(uri);
         ViewResolver.resolveView(uri, contentType, httpResponse);
-
-        // 컨텐트 타입에 해당하는 확장자가 있다면,
-        // accept에 해당하는 확장자가 있다면,
-
-        // 정적 페이지를 처리하는 것인지, 아닌지를 해결하자.
-
-//        if (uri.startsWith("/user/create")) {
-//            String userId = httpRequest.getQueryParameter("userId");
-//            String password = httpRequest.getQueryParameter("password");
-//            String name = httpRequest.getQueryParameter("name");
-//            String email = httpRequest.getQueryParameter("email");
-//            UserService.join(userId, password, name, email);
-//
-//            byte[] body = httpResponse.readFile("/index.html", contentType);
-//            httpResponse.response200Header(body.length);
-//            httpResponse.responseBody(body);
-//        }
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -21,12 +21,16 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
+            // InputStreamReader 통해서 HttpRequest 클래스를 만든다
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             HttpRequest httpRequest = new HttpRequest(br);
 
+            // DataOutputStream 통해서 HttpResponse 클래스를 만든다
             DataOutputStream dos = new DataOutputStream(out);
             HttpResponse httpResponse = new HttpResponse(dos);
 
+            // FrontController 클래스를 만든다
             FrontController frontController = new FrontController();
             frontController.handleRequest(httpRequest, httpResponse);
 
