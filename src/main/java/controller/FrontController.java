54e727f57b5046adf82fa6e195e4a6cf98e2d05b
kimdongmin <meena2003@naver.com>
2023-04-19 11:16:47 +0900

    refactor(mission4): FrontController에서 중복 코드 제거
    View를 만드는 기능을 통일함

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -21,6 +21,7 @@ public class FrontController {
             uri = "/index.html";
         }
 
+        // uri가 "/user/create"라면 회원 가입
         if (uri.startsWith("/user/create")) {
             String userId = httpRequest.getQueryParameter("userId");
             String password = httpRequest.getQueryParameter("password");
@@ -28,15 +29,8 @@ public class FrontController {
             String email = httpRequest.getQueryParameter("email");
             UserService.join(userId, password, name, email);
 
-            // 다시 홈 화면을 출력한다.
+            // 다시 홈 화면 uri를 요청한다.
             uri = "/index.html";
-            ContentType contentType = ContentType.getContentType(uri);
-            ViewResolver.resolveView(uri, contentType, httpResponse);
-
-            byte[] body = httpResponse.readFile(contentType.getRootPath(), uri);
-            httpResponse.response200Header(body.length, contentType);
-            httpResponse.responseBody(body);
-            return;
         }
 
         // 요청받은 파일을 처리한다
